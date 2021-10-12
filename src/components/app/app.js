77cc773458213268bdbf2792b97d92cd3d6ab3bc
Olena Vdovych <olenavdovych17@gmail.com>
2021-10-12 23:35:02 +0300

    Added functionality when you clicked to the button pannier you are deleted the employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,18 +1,37 @@
-import './app.css';
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list'
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
-function App() {
+import './app.css';
 
-    const data = [
-        {name: 'John S.', salary: 800, increase: false, id: 1},
-        {name: 'Michael K.', salary: 3000, increase: false, id: 2},
-        {name: 'Carl R.', salary: 4500, increase: true, id: 3}
-    ];
+class App extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                {name: 'John S.', salary: 800, increase: false, id: 1},
+                {name: 'Michael K.', salary: 3000, increase: false, id: 2},
+                {name: 'Carl R.', salary: 4500, increase: true, id: 3}
+            
+        ]
+        }
+    }
+
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+
+render() {
     return (
         <div className="app">
             <AppInfo/>
@@ -21,10 +40,14 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-            <EmployeesList data={data}/>
+            <EmployeesList 
+            data={this.state.data}
+            onDelete={this.deleteItem}/>
             <EmployeesAddForm/>
         </div>
     );
+}
+
 }
 
 export default App;
