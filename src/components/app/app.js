7c0478958f8555ba894875e3d705077b7eec18f1
Olena Vdovych <olenavdovych17@gmail.com>
2021-10-15 21:40:17 +0300

    Added functionality: you can added a new employee and salary, you can see in the top information about number of employyees and who will be received the award

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -14,27 +14,56 @@ class App extends Component {
         super(props);
         this.state = {
             data: [
-                {name: 'John S.', salary: 800, increase: false, id: 1},
-                {name: 'Michael K.', salary: 3000, increase: false, id: 2},
-                {name: 'Carl R.', salary: 4500, increase: true, id: 3}
+                {name: 'John S.', salary: 800, increase: false, rise: true, id: 1},
+                {name: 'Michael K.', salary: 3000, increase: false, rise: false, id: 2},
+                {name: 'Carl R.', salary: 4500, increase: true, rise: false, id: 3}
             
-        ]
+            ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
-
             return {
                 data: data.filter(item => item.id !== id)
             }
         })
     }
 
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            rise: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    onToggleProp = (id, prop) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, [prop]: !item[prop]}
+                }
+                return item;
+            })
+        }))
+    }
+
 render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
     return (
         <div className="app">
-            <AppInfo/>
+            <AppInfo employees={employees} increased={increased}/>
 
             <div className="search-panel">
                 <SearchPanel/>
@@ -42,8 +71,10 @@ render() {
             </div>
             <EmployeesList 
             data={this.state.data}
-            onDelete={this.deleteItem}/>
-            <EmployeesAddForm/>
+            onDelete={this.deleteItem}
+            onToggleProp={this.onToggleProp}/>
+            <EmployeesAddForm
+            onAdd={this.addItem}/>
         </div>
     );
 }
