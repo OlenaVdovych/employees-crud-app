6ad48bd43a19895a666bb979c54269a9cd651e77
Olena Vdovych <olenavdovych17@gmail.com>
2021-10-15 23:20:20 +0300

    Added functionality when you can to search employee

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,8 +17,8 @@ class App extends Component {
                 {name: 'John S.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Michael K.', salary: 3000, increase: false, rise: false, id: 2},
                 {name: 'Carl R.', salary: 4500, increase: true, rise: false, id: 3}
-            
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -58,19 +58,36 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
 render() {
+    const {data, term} = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.searchEmp(data, term); 
+
     return (
         <div className="app">
             <AppInfo employees={employees} increased={increased}/>
 
             <div className="search-panel">
-                <SearchPanel/>
+                <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                 <AppFilter/>
             </div>
             <EmployeesList 
-            data={this.state.data}
+            data={visibleData}
             onDelete={this.deleteItem}
             onToggleProp={this.onToggleProp}/>
             <EmployeesAddForm
